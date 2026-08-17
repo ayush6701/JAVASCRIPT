71596bd9add155614433013b8178a1502f13eece
ayush6701 <ayush20lv@gmail.com>
2026-08-18 00:12:36 +0530

    global and local scope

---
+++ functions/02_scopes.js
@@ -0,0 +1,24 @@
+function one(){
+    const username = "Ayush"
+    function two(){
+        const website = "Google"
+        // console.log(username)
+        // console.log(website)
+        
+    }
+    two()
+    // console.log(username)
+    // console.log(website)
+}
+one()
+
+if (true) {
+    const username = "ayush"
+    if (username === "ayush") {
+        const website = "Google"
+        console.log(usename + website);
+        // console.log(website);
+    }
+    // console.log(username);
+    // console.loh(website);
+}
