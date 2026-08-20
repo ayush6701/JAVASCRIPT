e1c2cc7f2f2932dddac7ad9a86ef4a740f85cad3
ayush6701 <ayush20lv@gmail.com>
2026-08-20 16:27:06 +0530

    switch & break

---
+++ control_flow/switch.js
@@ -0,0 +1,50 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default: 
+        console.log("no match")
+        break;
+}
+
+const monthName = "mar"
+
+switch (monthName) {
+    case "jan":
+        console.log("1")
+        break;
+    case "feb":
+        console.log("2")
+        break;
+    case "mar":
+        console.log("3")
+        break;
+    case "apr":
+        console.log("4")
+        break;
+
+    default:
+        console.log("none")
+        break;
+}
